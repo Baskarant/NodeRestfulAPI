4e1f254b381c418c0ba3a9d70bba909c24c60530
baskarant <baskar159@gmail.com>
2017-07-27 09:57:05 +0530

    code for helloworld message localhost:3000/api

--- server.js
+++ server.js
@@ -1,22 +1,21 @@
 var express = require('express');
 var app = express();
-var bodyParser = require('bodyParser');
+var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 
 // configure app for bodyParser()
 // let grab data from the body  of post
-
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 // Set up part for server to listen on
 var port = process.env.PORT || 3000;
 
 // connect to database
-mongoose.connect('mongodb://localhost:27017/codelong');
+mongoose.createConnection('mongodb://localhost:27017/codelong');
 
 // api routes
-var router =  express.Route();
+var router =  express.Router();
 
 // routes will all be preferred with api
 app.use('/api',router);
@@ -27,3 +26,7 @@ router.get('/',function(req,res){
 });
 
 // fire up server
+app.listen(port);
+
+//print friendly message
+console.log('Server is listening on port' +port);
